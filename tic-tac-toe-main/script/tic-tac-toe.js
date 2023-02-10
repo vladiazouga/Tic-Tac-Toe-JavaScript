ba5216e277a0615800db8fb0110d93675747c6b7
vladiazouga <vzouga2021@fau.edu>
2023-02-09 23:54:11 -0500

    added skeleton code (comments)

--- tic-tac-toe-main/script/tic-tac-toe.js
+++ tic-tac-toe-main/script/tic-tac-toe.js
@@ -7,19 +7,37 @@ let compScore = document.querySelector('#compScore')
 let compWins = 0;
 //Tracks the wins of the computer and updates the scoreboard
 let playerWins = 0;
+let currentPlayer = "X"
+//Chooses what symbol the current player gets
+/*let playerSymbol = Math.random();
+console.log(playerSymbol);
 let currentPlayer;
+if (playerSymbol >= .2){ currentPlayer = "X";}
+if (playerSymbol < .2){ currentPlayer = "O"}*/
+
 //Have a place holder so the human player can always go first
 let humanPlayer = currentPlayer;
-//Randomly choses which symbol (X or O) the 
-let playerSymbol = Math.random()
+
 //If human player has an X symbol then computer has O symbol vice versa
 //Always makes sure computer's symbol is opposites of human player
 let computer = humanPlayer === "X" ? "O" : "X";
-let gameActive = true;
 
+let gameActive = true;
+console.log(currentPlayer)
 let gameState = ["", "", "", "", "", "", "", "", ""];
 
+//Shows what cells are open that haven't been chosen by a player
+var openCell=new Set();
+for(let i=0;i<9;i++){
+    openCell.add(i)
+}
 
+console.log(openCell);
+
+/*let computerTurn=()=>{
+    var arr =  
+
+}*/
 
 const winningMessage = () => `Player ${currentPlayer} has won!`;
 const drawMessage = () => `Game ended in a draw!`;
@@ -38,6 +56,7 @@ const winningConditions = [
     [2, 4, 6]
 ];
 
+//This handles the updating of the board of who just played
 function handleCellPlayed(clickedCell, clickedCellIndex) {
     gameState[clickedCellIndex] = currentPlayer;
     clickedCell.innerHTML = currentPlayer;
@@ -48,10 +67,14 @@ function handlePlayerChange() {
     statusDisplay.innerHTML = currentPlayerTurn();
 }
 
+
+
 function handleResultValidation() {
     let roundWon = false;
+    //Checks every winning combo to see if it was filled
     for (let i = 0; i <= 7; i++) {
         const winCondition = winningConditions[i];
+        // Checks each row to see whether the same a,b,c is empty or filled with the same symbol
         let a = gameState[winCondition[0]];
         let b = gameState[winCondition[1]];
         let c = gameState[winCondition[2]];
@@ -68,24 +91,68 @@ function handleResultValidation() {
         statusDisplay.innerHTML = winningMessage();
         gameActive = false;
         statusDisplay.style.color = "rgb(251,100,204)";
-        return;
+        return; //roundWon (this is because its a boolean true variable);
+        
     }
 
+    //Checks if there is any empty spaces 
     let roundDraw = !gameState.includes("");
     if (roundDraw) {
         statusDisplay.innerHTML = drawMessage();
         gameActive = false;
         statusDisplay.style.color = "rgb(251,100,204)";
-        return;
+        return; //false;
     }
 
+    //CheckWin();
     handlePlayerChange();
+    
+    //ComputerMove();
+    
 }
+
+/*function ComputerMove(){
+    
+    //First has to pick the best move
+    pickComputerMove()
+    //Check if the the computer made a winning combo
+    CheckWin()
+    //Goes to the next player (human)
+    handlePlayerChange()
+
+
+}
+*/
+
+/*function pickComputerMove(){
+    //Will loop through gameState array until and find a random available spot
+    while(true){
+        //Finds a random number between 1 and 9
+        m = Math.floor(Math.random()*8))
+        if(gameState[m] == '') //Checks if its empty
+        //Check through gameState and an available spot 
+
+    }
+    //Will set the computer move
+    //
+    gameState[m] = currentPlayer
+    document.getElementById(m).innerhtml = currentPlayer
+
+}
+
+function checkWin(){
+
+}
+
+
+*/
+ 
 
 function handleCellClick(clickedCellEvent) {
     const clickedCell = clickedCellEvent.target;
     const clickedCellIndex = parseInt(clickedCell.getAttribute('data-cell-index'));
 
+    //checks if the cell index is not empty 
     if (gameState[clickedCellIndex] !== "" || !gameActive) {
         return;
     }
@@ -102,6 +169,6 @@ function handleRestartGame() {
     statusDisplay.innerHTML = currentPlayerTurn();
     document.querySelectorAll('.cell').forEach(cell => cell.innerHTML = "");
 }
-
+//Adds event listener to each cell id
 document.querySelectorAll('.cell').forEach(cell => cell.addEventListener('click', handleCellClick));
 document.querySelector('.restart').addEventListener('click', handleRestartGame);
